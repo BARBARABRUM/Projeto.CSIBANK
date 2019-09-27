8bfca53831d277aae20eff69004e1976b3d408c5
Barbara <bbrumfoto@gmail.com>
2019-09-26 21:48:47 -0300

    TestaMetodo

---
+++ CSIBank/src/TestaMetodo.java
@@ -0,0 +1,14 @@
+
+public class TestaMetodo {
+
+	public static void main(String[] args) {
+		Conta conta1 = new Conta();
+		conta1.deposita(100);
+		conta1.deposita(50);
+		
+		System.out.println("Saldo" + conta1.saldo);
+		
+
+	}
+
+}
